54732bf4b558e1b984d4859a101987d7887f2087
Vlad-Pelmeshka <vcheredaiko@gmail.com>
2024-02-19 15:17:06 +0200

    Correct finish main part

--- cli.php
+++ cli.php
@@ -10,7 +10,8 @@ class VendingMachine
         '0.50',
         '1.00',
     );
-    private $count_roducts = 0;
+    private $count_products = 0;
+    private $total_money_input = 0;
     private $currency = '$';
     private $selected_product = '';
     
@@ -18,12 +19,12 @@ class VendingMachine
         '0' => array(
             'title' => "Coca-cola",
             'price' => 1.5,
-            'count' => 3,
+            'count' => 2,
         ),
         '1' => array(
             'title' => "Snickers",
             'price' => 1.2,
-            'count' => 6,
+            'count' => 2,
         ),
         '2' => array(
             'title' => "Lay's",
@@ -37,47 +38,127 @@ class VendingMachine
         
     }
 
-    public function createOrder() {
+    public function __destruct() {
+        // self::showHeader();
+        print "We look forward to seeing you again with your money. Thank you for choosing us!\n";
+    }
+
+    public function createOrder($first_order = true) {
 
         if(self::isProductsInMachine() != 0){
             
-            do { 
-                
-                print "Now u can select to buy next products:\n";
-                
-                self::displayProductList();
+            if($first_order){
+                do { 
+                    
+                    print "Now u can select to buy next products:\n";
+                    
+                    self::displayProductList();
 
-                print "\nAre you ready to make an order? (Press 'Y' or 'N'): \n";
+                    print "\nAre you ready to make an order? (Press 'Y' or 'N'): \n";
 
-                flush();
-                $input = fread(STDIN,1);
+                    flush();
+                    $input = fread(STDIN,1);
+                    
                 
-            
-                if ($input == 'Y' || $input == 'y') {
-                    break;
-                }
-            
-                if ($input == 'N' || $input == 'n') {
+                    if ($input == 'Y' || $input == 'y') {
+                        break;
+                    }
+                
+                    if ($input == 'N' || $input == 'n') {
+                        
+                        self::showHeader();
+                        return;
+                    }
                     
                     self::showHeader();
-                    print "Looking forward to seeing you next time\n";
+                    print "Sorry, but your entry is incorrect. ";
+                } while (true);
+            }
+
+            self::selectProduct();
+            
+            self::inputMoney();
+
+            print "Please get your " . self::getProductTitle($this->selected_product) . ".\n";
+            do { 
+                print "Your change: " . $this->currency . number_format($this->total_money_input,2) . ". Would you like to choose something else? (Press 'Y' or 'N'):\n";
+                flush();
+                $input = fread(STDIN,1);
+
+                if ($input == 'Y' || $input == 'y') {
+                    self::createOrder(false);
                     return;
                 }
                 
                 self::showHeader();
-                print "Sorry, but your entry is incorrect. ";
+            
+                if ($input == 'N' || $input == 'n') {
+                    self::returnUserChange();
+                    return;
+                }
             } while (true);
 
-            self::selectProduct();
-            
 
         }else{
+            self::showHeader();
             print "Sorry, but all of our products have now been sold out.\n";
+            self::returnUserChange();
         }
         
-        self::showHeader();
-        print "We look forward to seeing you again with your money. Thank you for choosing us!\n";
         
+    }
+    
+    private function returnUserChange(){
+        if($this->total_money_input > 0){
+            print "Please take your change: " . $this->currency . number_format($this->total_money_input,2) . "\n";
+        }
+    }
+
+    private function inputMoney(){
+        $total_money_input = $this->total_money_input;
+        $total_money_need = self::getProductPrice($this->selected_product);
+        
+        self::showHeader();
+
+        do{
+            print "You chose " . self::getProductTitle($this->selected_product);
+            print "\nProduct cost:       " . $this->currency . number_format($total_money_need, 2);
+            print "\nMoney deposited:    " . $this->currency . number_format($total_money_input, 2);
+            print "\nRemains to deposit: " . $this->currency . number_format($total_money_need - $total_money_input, 2);
+
+            print "\n\nDeposit money: " ;
+            
+            flush();
+            $input = fread(STDIN,10); 
+
+            self::showHeader();
+
+            if(!is_numeric($input) || !in_array(number_format($input, 2), $this->available_coins, true)){
+                
+                self::showHeader();
+                print "The device could not recognize your input. Please use only the following coins/bills:\n";
+
+                self::getCorrectCoins();
+                continue;
+            }
+
+            $total_money_input += (float)$input;
+
+        }while($total_money_need > $total_money_input);
+
+        $this->total_money_input = $total_money_input - $total_money_need;
+
+        self::reduceProductCount($this->selected_product);
+        self::reduceTotalCount();
+    }
+
+    private function getCorrectCoins(){
+        $currency = $this->currency;
+        $coins_with_currency = array_map(function($coin) use ($currency) {
+            return $currency . number_format($coin, 2);
+        }, $this->available_coins);
+        
+        print implode(', ', $coins_with_currency) . "\n\n";
     }
 
     private function showHeader(){
@@ -90,7 +171,7 @@ class VendingMachine
         print "Lets do it! ";
 
         do {
-            print "Please choose one of the products (Input his number): \n";
+            print "Please choose one of the products (Input his code): \n";
 
             self::displayProductList(true);
 
@@ -98,7 +179,7 @@ class VendingMachine
             $input = fread(STDIN,10); 
             $input = trim($input);
 
-            if (!is_numeric($input) || !self::getProductById($input)) {
+            if (!self::getProductById($input)) {
                 
                 self::showHeader();
                 print "This option is not in the list. ";
@@ -123,7 +204,7 @@ class VendingMachine
                 continue;
             }
 
-            print ($show_id ? $key . ') ' : '') . $this-> currency . number_format($product['price'],2) . " " . $product['title'] . "\n";
+            print ($show_id ? $key . ': ' : '') . $this->currency . number_format($product['price'],2) . " " . $product['title'] . "\n";
         }
         
         return $this->available_products;
@@ -131,16 +212,16 @@ class VendingMachine
 
     public function isProductsInMachine(){
 
-        if($this->count_roducts == 0){
-            $count_roducts = 0;
+        if($this->count_products == 0){
+            $count_products = 0;
             
             foreach($this->available_products as $product){
-                $count_roducts += $product['count'];
+                $count_products += $product['count'];
             }
-            $this->count_roducts = $count_roducts;
+            $this->count_products = $count_products;
         }
 
-        return $this->count_roducts;
+        return $this->count_products;
     }
 
     private function cls(){
@@ -148,11 +229,11 @@ class VendingMachine
     }
 
     private function getProductById($id){
-        var_dump($id);
+        // var_dump($id);
         return $this->available_products[$id] ?? '';
     }
 
-    /*public function getProductTitle($id){
+    public function getProductTitle($id){
         $data = self::getProductById($id);
         return $data ? $data['title'] : '';
     }
@@ -162,10 +243,13 @@ class VendingMachine
         return $data ? $data['price'] : '';
     }
 
-    public function getProductCount($id){
-        $data = self::getProductById($id);
-        return $data ? $data['count'] : '';
-    }*/
+    public function reduceProductCount($id){
+        $this->available_products[$id]['count']--; 
+    }
+
+    public function reduceTotalCount(){
+        $this->count_products--; 
+    }
 }
 
 $VM = new VendingMachine();
